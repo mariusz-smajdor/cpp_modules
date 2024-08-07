303fbce197c8e2bf272378469fe0f6ea9e6bdd1e
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-08-07 08:28:35 +0000

    add nickname and darkest secret class fields

--- cpp_module_00/ex01/Contact/Contact.class.cpp
+++ cpp_module_00/ex01/Contact/Contact.class.cpp
@@ -2,7 +2,9 @@
 
 Contact::Contact(void) : _firstname(""),
 						_lastname(""),
-						_number("") {
+                        _nickname(""),
+						_number(""), 
+                        _darkestSecret("") {
 	return ;
 }
 
@@ -20,8 +22,16 @@ void Contact::setLastname(std::string str) {
     this->_lastname = str;
 }
 
+void Contact::setNickname(std::string str) {
+    this->_nickname = str;
+}
+
 void Contact::setNumber(std::string str) {
     this->_number = str;
+}
+
+void Contact::setDarkestSecret(std::string str) {
+    this->_darkestSecret = str;
 }
 
 // GETTERS
@@ -34,6 +44,14 @@ std::string Contact::getLastname(void) const {
     return this->_lastname;
 }
 
+std::string Contact::getNickname(void) const {
+   return this->_nickname;
+}
+
 std::string Contact::getNumber(void) const {
     return this->_number;
+}
+
+std::string Contact::getDarkestSecret(void) const {
+    return this->_darkestSecret;
 }
--- cpp_module_00/ex01/Contact/Contact.class.hpp
+++ cpp_module_00/ex01/Contact/Contact.class.hpp
@@ -7,18 +7,23 @@ class	Contact {
 	private:
 		std::string	_firstname;
 		std::string	_lastname;
+		std::string	_nickname;
 		std::string	_number;
-	
+		std::string	_darkestSecret;
+
 	public:
 		Contact(void);
 		~Contact(void);
 
 		const std::string getFirstname(void) const;
 		const std::string getLastname(void) const;
+		const std::string getNickname(void) const;
 		const std::string getNumber(void) const;
+		const std::string getDarkestSecret(void) const;
 		void setFirstname(std::string str);
 		void setLastname(std::string str);
 		void setNumber(std::string const str);
+		void setNickname(std::string const str);
 };
 
 #endif
