dff119cbfe03535de07e31205345253a7d167a85
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-08-09 16:06:55 +0000

    create stack zombie

--- cpp_module_01/ex_00/main.cpp
+++ cpp_module_01/ex_00/main.cpp
@@ -1,12 +1,16 @@
 #include "Zombie.hpp"
 
 Zombie* newZombie(const std::string& name);
+void randomChump(const std::string& name);
 
 int main() {
     // create a zombie on the heap
     Zombie* heapZombie = newZombie("HeapZombie");
     heapZombie->announce();
     delete heapZombie; // free the heap memory
+
+    // create a zombie on the stack
+    randomChump("StackZombie");
 
     return 0;
 }
---
+++ cpp_module_01/ex_00/randomChump.cpp
@@ -0,0 +1,6 @@
+#include "Zombie.hpp"
+
+void randomChump(const std::string& name) {
+    Zombie zombie(name);
+    zombie.announce();
+}
