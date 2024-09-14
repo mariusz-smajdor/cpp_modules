4f051c6834e35601e9a0d63853ea19c3ec816543
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-09-14 11:08:54 +0000

    change ClapTrap init values

--- cpp_module_03/ex01/ClapTrap.cpp
+++ cpp_module_03/ex01/ClapTrap.cpp
@@ -3,9 +3,9 @@
 // Constructor and Destructor
 
 ClapTrap::ClapTrap(std::string name) : _name(name),
-                                       _hitPoints(10),
-                                       _energyPoints(10),
-                                       _attackDamage(0) {
+                                       _hitPoints(100),
+                                       _energyPoints(50),
+                                       _attackDamage(20) {
     std::cout << name << " was created!" << std::endl; 
 }
 
