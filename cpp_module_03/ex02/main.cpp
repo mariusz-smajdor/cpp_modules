a13b51496050ef8be2261ae137a5e91fb48a2b5b
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-09-14 11:50:29 +0000

    add FragTrap class
    cpp03ex02

--- cpp_module_03/ex02/main.cpp
+++ cpp_module_03/ex02/main.cpp
@@ -1,4 +1,5 @@
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main() {
     ScavTrap Super("Super");
@@ -6,5 +7,13 @@ int main() {
     Super.takeDamage(4);
     Super.beRepaired(1);
     Super.guardGate();
+
+    std::cout << "\n\n\n";
+
+    FragTrap Super2("Super2");
+    Super2.attack("Maciej 🤡");
+    Super2.takeDamage(3);
+    Super2.beRepaired(2);
+    Super2.highFivesGuys();
     return 0;
 }
